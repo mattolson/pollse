5926a3afb73596fbba7eefdaeadf8cd0bdb23366
Patrick Edelman <patrick.the.edelman@gmail.com>
2013-10-18 21:22:08 -0700

    initial commit

---
+++ pollse/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pollse::Application.config.secret_token = 'ee08f4496000c8a09119d261448a2a668dafb84019bef988ba17903711ec15dc7cefd349415c083212ccd3b334c4579638ff1db7e82f90f8dd882b61f381333f'
