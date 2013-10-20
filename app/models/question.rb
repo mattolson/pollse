27621c09182658b0a968104aeab7dff27195219e
Chris Morris <HitsvilleUSA@yahoo.com>
2013-10-19 17:34:23 -0700

    adding validations on question_options

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,16 +1,17 @@
 class Question < ActiveRecord::Base
   belongs_to :user
   
-  has_many   :question_options,     :dependent => :destroy
-  has_many   :polls,                :dependent => :destroy
-  has_many   :category_assignments, :dependent => :destroy
-  has_many   :categories,           :through   => :category_assignments
+  has_many   :question_options,     :dependent  => :destroy,
+                                    :inverse_of => :question
+  has_many   :polls,                :dependent  => :destroy
+  has_many   :category_assignments, :dependent  => :destroy
+  has_many   :categories,           :through    => :category_assignments
   
   validates :user_id, :presence => true
   validates :body,    :presence => true
   
   accepts_nested_attributes_for :question_options, :allow_destroy => true, 
-                                                   :reject_if     => proc { |attributes| attributes['response_value'].blank? }
+                                                   :reject_if     => proc { |attrs| attrs['response_value'].blank? }
   
   attr_accessible :body,
                   :question_options_attributes
--- app/models/question_option.rb
+++ app/models/question_option.rb
@@ -1,6 +1,9 @@
 class QuestionOption < ActiveRecord::Base
   belongs_to :question
   
+  validates :question,       :presence   => true
+  validates :response_value, :presence   => true
+  
   attr_accessible :response_value
   
   def to_s
