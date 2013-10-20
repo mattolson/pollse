80fcc1277bb0114e7b17b5e64334529c19afc0fe
Matt Olson <matt@mattolson.com>
2013-10-20 10:25:53 -0700

    Use speech bubble for poll partial
    -- Also apply partial to home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController < ApplicationController
 
   def index
+    @featured_poll = Poll.where(:featured => true).first
+    if @featured_poll
+      @response = @featured_poll.responses.build
+    end
     render layout: "home"
   end
 
