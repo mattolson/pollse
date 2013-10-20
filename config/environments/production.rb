d0d17d0966013f021fb09506fbbe7a674198f1d7
Chris Morris <HitsvilleUSA@yahoo.com>
2013-10-20 15:01:04 -0700

    Adding default url host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ Pollse::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => "tectonics.r13.railsrumble.com" }
 
   # Enable threaded mode
   # config.threadsafe!
